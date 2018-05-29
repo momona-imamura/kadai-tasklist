c1cc3a853caff1cf9175fa64dc45ec48bff61955
momona.imamura <ec2-user@ip-172-31-17-5.us-east-2.compute.internal>
2018-05-29 04:10:12 +0000

    task model

--- database/migrations/2018_05_29_103107_create_tasks_table.php
+++ database/migrations/2018_05_29_103107_create_tasks_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatetasksTable extends Migration
+class CreateTasksTable extends Migration
 {
     /**
      * Run the migrations.
@@ -15,7 +15,7 @@ class CreatetasksTable extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('content'); 
+            $table->string('content');  
             $table->timestamps();
         });
     }
