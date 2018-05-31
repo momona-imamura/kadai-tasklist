be16e4b5bacaece816fe91ed07812c594ee15b2e
momona.imamura <ec2-user@ip-172-31-17-5.us-east-2.compute.internal>
2018-05-31 01:41:33 +0000

    add column

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -2,11 +2,15 @@
 
 @section('content')
 
-<h1>メッセージ新規作成ページ</h1>
+<h1>タスク新規作成ページ</h1>
 
     {!! Form::model($task, ['route' => 'tasks.store']) !!}
 
-        {!! Form::label('content', 'メッセージ:') !!}
+        {!! Form::label('status', 'ステータス:') !!}
+        {!! Form::text('status') !!}
+
+
+        {!! Form::label('content', 'タスク:') !!}
         {!! Form::text('content') !!}
 
         {!! Form::submit('投稿') !!}
--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -5,7 +5,8 @@
 
     <h1>id = {{ $task->id }} のタスク詳細ページ</h1>
 
-    <p>{{ $task->content }}</p>
+    <p>タスク{{ $task->content }}</p>
+    <p>タスク{{ $task->status }}</p>
     
      {!! link_to_route('tasks.edit', 'このタスクを編集', ['id' => $task->id]) !!}
 
